05bd4e2a0baa4895ed11d552eeba1d3fd23540eb
Hufung <102956239+Hufung@users.noreply.github.com>
2025-11-03 22:15:38 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}"          // Also look in the root
+  ], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
